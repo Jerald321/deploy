eccc4e957db8b90d910371becd8ad04f0638db5b
Jerald321 <rjerald6803@gmail.com>
2024-11-03 19:24:34 +0530

    start-1

---
+++ src/component/Header.jsx
@@ -0,0 +1,19 @@
+
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+      
+          {/* <Link to="/">Header</Link> */}
+          <Link to="/home">Home</Link>
+    </div>
+ 
+      
+  
+
+  
+  )
+}
+
+export default Header;
---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores quos rem inventore aliquid porro corporis sed officia optio provident impedit asperiores eligendi, ipsum minima voluptatum quasi, repellat eius et sapiente!</h1>
+    </div>
+  )
+}
+
+export default Home
